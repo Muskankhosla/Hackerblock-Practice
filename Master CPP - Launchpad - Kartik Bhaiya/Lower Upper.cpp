3f6af6be1b8b33059e80f76ece68f758a9928a2a
Muskan Khosla <khosla.muskan01@gmail.com>
2021-10-08 13:46:49 +0530

    Create Lower Upper.cpp

---
+++ Master CPP - Launchpad - Kartik Bhaiya/Lower Upper.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char n;
+	cin>>n;
+	if(n>='a'&&n<='z')
+	{
+		cout<<"lowercase";
+	}
+	else if(n>='A'&&n<='Z')
+	{
+		cout<<"UPPERCASE";
+	}
+	else
+	cout<<"Invalid";
+	return 0;
+}
